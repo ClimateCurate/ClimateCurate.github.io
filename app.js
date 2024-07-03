75730a47033b019bd34248117117128e9ed4fabe
Dhruv Saxena <justdhs@gmail.com>
2024-07-04 05:04:53 +0530

    Fixing Issue

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ let index = 0;
 
 function addCalendly(){
   console.log('typeWriter');
-  Calendly.initBadgeWidget({ url: 'https://calendly.com/justdhs/compliance-reporting-discussion', text: 'Schedule An Appointment With Us', color: '#0069ff', textColor: '#ffffff', branding: undefined }); 
   typeWriter()
 }
 function typeWriter() {
